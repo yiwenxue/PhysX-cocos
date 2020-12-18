efd96325e82fdc851fb29e8ca2aa2cfa579c5f60
JayceLai <goodlaijiaxin@qq.com>
2020-12-18 18:22:24 +0800

    advance filter shader & mesh scale

--- physx/source/physxwebbindings/src/PxWebBindings.cpp
+++ physx/source/physxwebbindings/src/PxWebBindings.cpp
@@ -150,28 +150,47 @@ PxFilterFlags DefaultFilterShader(
     PxFilterObjectAttributes attributes1, PxFilterData fd1,
     PxPairFlags &pairFlags, const void *, PxU32)
 {
+  // constexpr PxU32 QUERY_FILTER = 1 << 0;
+  // constexpr PxU32 QUERY_CHECK_TRIGGER = 1 << 1;
+  // constexpr PxU32 QUERY_SINGLE_HIT = 1 << 2;
+  constexpr PxU32 DETECT_TRIGGER_EVENT = 1 << 3;
+  constexpr PxU32 DETECT_CONTACT_EVENT = 1 << 4;
+  constexpr PxU32 DETECT_CONTACT_POINT = 1 << 5;
+  constexpr PxU32 DETECT_CONTACT_CCD = 1 << 6;
   // group mask filter
   if (!(fd0.word0 & fd1.word1) || !(fd0.word1 & fd1.word0))
   {
     return PxFilterFlag::eSUPPRESS;
   }
+  
+  pairFlags = PxPairFlags(0);
+  // need detect ccd contact?
+  const PxU16 needDetectCCD = (fd0.word3 & DETECT_CONTACT_CCD) | (fd1.word3 & DETECT_CONTACT_CCD);
+  if (needDetectCCD) pairFlags |= PxPairFlag::eDETECT_CCD_CONTACT;
+
   // trigger filter
   if (PxFilterObjectIsTrigger(attributes0) || PxFilterObjectIsTrigger(attributes1))
   {
-    pairFlags = PxPairFlag::eTRIGGER_DEFAULT |
-                PxPairFlag::eNOTIFY_TOUCH_FOUND |
-                PxPairFlag::eNOTIFY_TOUCH_LOST |
-                PxPairFlag::eNOTIFY_TOUCH_PERSISTS |
-                PxPairFlag::eDETECT_CCD_CONTACT;
+    pairFlags |= PxPairFlag::eDETECT_DISCRETE_CONTACT;
+
+    // need trigger event?
+    const PxU16 needTriggerEvent = (fd0.word3 & DETECT_TRIGGER_EVENT) | (fd1.word3 & DETECT_TRIGGER_EVENT);
+    if (needTriggerEvent) pairFlags |= PxPairFlag::eNOTIFY_TOUCH_FOUND | PxPairFlag::eNOTIFY_TOUCH_LOST | PxPairFlag::eNOTIFY_TOUCH_PERSISTS;
+
     return PxFilterFlag::eDEFAULT;
   }
+
   // simple collision process
-  pairFlags = PxPairFlag::eCONTACT_DEFAULT |
-              PxPairFlag::eNOTIFY_TOUCH_FOUND |
-              PxPairFlag::eNOTIFY_TOUCH_LOST |
-              PxPairFlag::eNOTIFY_TOUCH_PERSISTS |
-              PxPairFlag::eDETECT_CCD_CONTACT |
-              PxPairFlag::eNOTIFY_CONTACT_POINTS;
+  pairFlags |= PxPairFlag::eCONTACT_DEFAULT;
+
+  // need contact event?
+  const PxU16 needContactEvent = (fd0.word3 & DETECT_CONTACT_EVENT) | (fd1.word3 & DETECT_CONTACT_EVENT);
+  if (needContactEvent) pairFlags |= PxPairFlag::eNOTIFY_TOUCH_FOUND | PxPairFlag::eNOTIFY_TOUCH_LOST | PxPairFlag::eNOTIFY_TOUCH_PERSISTS;
+
+  // need contact point?
+  const PxU16 needContactPoint = (fd0.word3 & DETECT_CONTACT_POINT) | (fd1.word3 & DETECT_CONTACT_POINT);
+  if (needContactPoint) pairFlags |= PxPairFlag::eNOTIFY_CONTACT_POINTS;
+
   return PxFilterFlag::eDEFAULT;
 }
 
@@ -804,6 +823,10 @@ EMSCRIPTEN_BINDINGS(physx)
 
   class_<PxTriangleMeshGeometry, base<PxGeometry>>("PxTriangleMeshGeometry")
       .constructor<PxTriangleMesh *, const PxMeshScale &, PxMeshGeometryFlags>()
+      .function("setScale", optional_override([](PxTriangleMeshGeometry& geo, PxMeshScale& scale) {
+        geo.scale.scale = scale.scale;
+        geo.scale.rotation = scale.rotation;
+      }))
       .function("isValid", &PxTriangleMeshGeometry::isValid);
 
   class_<PxMeshGeometryFlags>("PxMeshGeometryFlags").constructor<int>();
@@ -816,9 +839,15 @@ EMSCRIPTEN_BINDINGS(physx)
       .function("release", &PxConvexMesh::release);
   class_<PxConvexMeshGeometry, base<PxGeometry>>("PxConvexMeshGeometry")
       .constructor<PxConvexMesh *, const PxMeshScale &, PxConvexMeshGeometryFlags>()
+      .function("setScale", optional_override([](PxConvexMeshGeometry& geo, PxMeshScale& scale) {
+        geo.scale.scale = scale.scale;
+        geo.scale.rotation = scale.rotation;
+      }))
       .function("isValid", &PxConvexMeshGeometry::isValid);
 
-  class_<PxMeshScale>("PxMeshScale").constructor<const PxVec3 &, const PxQuat &>();
+  class_<PxMeshScale>("PxMeshScale").constructor<const PxVec3 &, const PxQuat &>()
+      .function("setScale", optional_override([](PxMeshScale& ms, PxVec3& scale) { ms.scale = scale; }))
+      .function("setRotation", optional_override([](PxMeshScale& ms, PxQuat& rot) { ms.rotation = rot; }));
 
   class_<PxConvexMeshGeometryFlags>("PxConvexMeshGeometryFlags").constructor<int>();
   enum_<PxConvexMeshGeometryFlag::Enum>("PxConvexMeshGeometryFlag")
